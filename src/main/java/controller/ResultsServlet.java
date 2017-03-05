dc9fe14c53ea98d2787e3f7efc43b4675b626edc
MelikaAyoughi <melikaayoughi@gmail.com>
2017-03-05 09:42:20 +0330

    adding deploy.sh to easily deploy on tomcat + results servlet gets the flight list from domain

--- src/main/java/controller/ResultsServlet.java
+++ src/main/java/controller/ResultsServlet.java
@@ -1,31 +1,39 @@
 package controller;
 
 import domain.AkbarTicket;
+import domain.Flight;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 /**
  * Created by melikaayoughi on 3/4/17.
  */
+
 public class ResultsServlet extends HttpServlet {
 
     public void init() throws ServletException {
-//        AkbarTicket.getAkbarTicket();
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        AkbarTicket akbarTicket = AkbarTicket.getAkbarTicket();
+
         try {
             String source = request.getParameter("src");
             String destination = request.getParameter("dest");
             String departureDate = request.getParameter("departureDate");
             String returnDate = request.getParameter("returnDate");
-            String adultCount = request.getParameter("adult-count");
-            String childCount = request.getParameter("child-count");
-            String infantCount = request.getParameter("infant-count");
+            int adultCount = Integer.parseInt(request.getParameter("adult-count"));
+            int childCount = Integer.parseInt(request.getParameter("child-count"));
+            int infantCount = Integer.parseInt(request.getParameter("infant-count"));
+
+            ArrayList<Flight> flightsList = akbarTicket.search(source,destination,departureDate,adultCount,childCount,infantCount);
 
             request.getRequestDispatcher("Results.jsp").forward(request, response);
         }
