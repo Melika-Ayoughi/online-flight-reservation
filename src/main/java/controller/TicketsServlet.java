094451e3194325f6528dec1a0af953fcbebb45c4
MelikaAyoughi <melikaayoughi@gmail.com>
2017-03-07 17:42:16 +0330

    adding ticketServlet with it's mapping in web.xml

--- src/main/java/controller/TicketsServlet.java
+++ src/main/java/controller/TicketsServlet.java
@@ -1,7 +1,24 @@
 package controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by melikaayoughi on 3/7/17.
  */
-public class TicketsServlet {
+public class TicketsServlet extends HttpServlet{
+
+    public void init() throws ServletException {
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public void destroy() {
+    }
+
 }
