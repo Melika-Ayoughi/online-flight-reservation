c6980e71eda7a7e44f0aec434975c4c10566dbc0
Ali Iman <ali.iman.1995@gmail.com>
2017-03-10 13:16:07 +0330

    all 4 tests for ResultsServletTest completed successfully

--- src/test/java/controller/ResultsServletTest.java
+++ src/test/java/controller/ResultsServletTest.java
@@ -127,6 +127,75 @@ public class ResultsServletTest {
         Elements responseElements = responseDoc.getElementsByClass("results-form");
         assertEquals("There are 2 flights and 2 seatClasses that can handle 9 persons",
                 2, responseElements.size());
+
+        Element responseOneElement = responseElements.get(0);
+        Element responseTwoElement = responseElements.get(1);
+        Elements elements;
+
+        elements = responseOneElement.getElementsByClass("col-md-3 place-middle font-large lbl-color-black pull-right");
+        Element airlineCodeFlightNumber = elements.get(0);
+        String airlineCodeFlightNumberString = airlineCodeFlightNumber.childNode(0).toString();
+        assertEquals("Flight number and airline code", "\n IR 452 ", airlineCodeFlightNumberString);
+
+        elements = responseOneElement.getElementsByClass("col-md-9 place-middle font-large lbl-color-dark-gray pull-right");
+        Element srcCodeDestCodeArrTimeDepTime = elements.get(0);
+        String srcCodeDestCodeArrTimeDepTimeString = srcCodeDestCodeArrTimeDepTime.childNode(0).toString();
+        assertEquals("SrcCode destCode arrTime depTime", "\n THR 1740 &gt;&gt; MHD 1850 ", srcCodeDestCodeArrTimeDepTimeString);
+
+        elements = responseOneElement.getElementsByClass("col-md-3 place-middle lbl-color-black pull-right");
+        Element date = elements.get(0);
+        String dateString = date.childNode(3).toString();
+        assertEquals("Date", "<span>05Feb</span>", dateString);
+
+        elements = responseOneElement.getElementsByClass("col-md-3 place-middle font-small lbl-color-light-gray pull-right");
+        Element planeModel = elements.get(0);
+        String plainModelString = planeModel.childNode(3).toString();
+        assertEquals("Plain model", "<span>M80</span>", plainModelString);
+
+        elements = responseOneElement.getElementsByClass("col-md-6 place-middle font-small lbl-color-light-gray pull-right");
+        Element seatClassCapacity = elements.get(0);
+        String capacityString = seatClassCapacity.childNode(3).toString();
+        String seatClassString = seatClassCapacity.childNode(7).toString();
+        assertEquals("Capacity", "<span>9</span>", capacityString);
+        assertEquals("SeatClass name", "<span> M </span>", seatClassString);
+
+        elements = responseOneElement.getElementsByClass("col-md-12 place-middle");
+        Element totalPrice = elements.get(0);
+        String totalPriceString = totalPrice.childNode(1).toString();
+        assertEquals("Total price = 4*4000 + 3*3000 + 2*2000", "<span>29000</span>", totalPriceString);
+
+
+        elements = responseTwoElement.getElementsByClass("col-md-3 place-middle font-large lbl-color-black pull-right");
+        airlineCodeFlightNumber = elements.get(0);
+        airlineCodeFlightNumberString = airlineCodeFlightNumber.childNode(0).toString();
+        assertEquals("Flight number and airline code", "\n IR 822 ", airlineCodeFlightNumberString);
+
+        elements = responseTwoElement.getElementsByClass("col-md-9 place-middle font-large lbl-color-dark-gray pull-right");
+        srcCodeDestCodeArrTimeDepTime = elements.get(0);
+        srcCodeDestCodeArrTimeDepTimeString = srcCodeDestCodeArrTimeDepTime.childNode(0).toString();
+        assertEquals("SrcCode destCode arrTime depTime", "\n THR 0730 &gt;&gt; MHD 0840 ", srcCodeDestCodeArrTimeDepTimeString);
+
+        elements = responseTwoElement.getElementsByClass("col-md-3 place-middle lbl-color-black pull-right");
+        date = elements.get(0);
+        dateString = date.childNode(3).toString();
+        assertEquals("Date", "<span>05Feb</span>", dateString);
+
+        elements = responseTwoElement.getElementsByClass("col-md-3 place-middle font-small lbl-color-light-gray pull-right");
+        planeModel = elements.get(0);
+        plainModelString = planeModel.childNode(3).toString();
+        assertEquals("Plain model", "<span>351</span>", plainModelString);
+
+        elements = responseTwoElement.getElementsByClass("col-md-6 place-middle font-small lbl-color-light-gray pull-right");
+        seatClassCapacity = elements.get(0);
+        capacityString = seatClassCapacity.childNode(3).toString();
+        seatClassString = seatClassCapacity.childNode(7).toString();
+        assertEquals("Capacity", "<span>9</span>", capacityString);
+        assertEquals("SeatClass name", "<span> F </span>", seatClassString);
+
+        elements = responseTwoElement.getElementsByClass("col-md-12 place-middle");
+        totalPrice = elements.get(0);
+        totalPriceString = totalPrice.childNode(1).toString();
+        assertEquals("Total price = 4*3000 + 3*2000 + 2*1000", "<span>20000</span>", totalPriceString);
     }
 
     @Test
