26445f686f0dcdda156977fb1d4acd3d177b493e
MelikaAyoughi <melikaayoughi@gmail.com>
2017-04-27 11:23:31 +0430

    working on second page, flightlist is now global to every controller, nested loop for flights and mapseaclasscapacity

--- src/main/webapp/js/App.js
+++ src/main/webapp/js/App.js
@@ -69,18 +69,79 @@ app.controller('mainController', function($scope, $http){
     $scope.message = 'main!';
 });
 
-app.controller('Home-SearchController', function($scope, $http, $location){
+app.controller('Home-SearchController', function($scope, $rootScope, $http, $location){
     $scope.message = 'search!';
-    $scope.flightList = [];
-    localThis = this;
+    $rootScope.flightList = [];
+    // localThis = this;
 
     this.searchFlights = function(){
-        $http.post('http://localhost:8080/online_flight_reservation/onlinereservation/searchService/getFlights',$scope.searchRequest).then(
-            function (response) {
-                localThis.flightList = response.data;
-                $location.path('/Results');
-            });
-    }
+        // $http.post('http://localhost:8080/online_flight_reservation/onlinereservation/searchService/getFlights',$scope.searchRequest).then(
+        //     function (response) {
+        //         $scope.flightList = response.data;
+        //         $location.path('/Results');
+        //     });
+
+        //just for testing
+        $rootScope.flightList = [{
+          "mapSeatClassCapacities": [{
+              "seatClass": {
+                  "name": "Y",
+                  "infantPrice": 1000,
+                  "adultPrice": 3000,
+                  "childPrice": 2000,
+                  "airlineCode": "IR",
+                  "originCode": "THR",
+                  "destinationCode": "MHD"
+              },
+              "capacity": 8
+          }, {
+              "seatClass": {
+                  "name": "M",
+                  "infantPrice": 2000,
+                  "adultPrice": 4000,
+                  "childPrice": 3000,
+                  "airlineCode": "IR",
+                  "originCode": "THR",
+                  "destinationCode": "MHD"
+              },
+              "capacity": 9
+          }],
+          "departureTime": "1740",
+          "arrivalTime": "1850",
+          "airplaneModel": "M80",
+          "flightNumber": "452",
+          "destCode": "MHD",
+          "srcCode": "THR",
+          "airlineCode": "IR",
+          "flightId": 0,
+          "date": "05Feb"
+      }, {
+          "mapSeatClassCapacities": [{
+              "seatClass": {
+                  "name": "F",
+                  "infantPrice": 1000,
+                  "adultPrice": 3000,
+                  "childPrice": 2000,
+                  "airlineCode": "IR",
+                  "originCode": "THR",
+                  "destinationCode": "MHD"
+              },
+              "capacity": 9
+          }],
+          "departureTime": "0730",
+          "arrivalTime": "0840",
+          "airplaneModel": "351",
+          "flightNumber": "822",
+          "destCode": "MHD",
+          "srcCode": "THR",
+          "airlineCode": "IR",
+          "flightId": 1,
+          "date": "05Feb"
+      }];
+        $location.path('/Results');
+
+
+    };
     $scope.searchRequest = {
         srcCode: 'THR',
         destCode: 'MHD',
@@ -91,8 +152,96 @@ app.controller('Home-SearchController', function($scope, $http, $location){
     };
 });
 
-app.controller('ResultsController', function($scope, $http){
+app.controller('ResultsController', function($scope, $rootScope, $http){
     $scope.message = 'results!';
+    var completeResult;
+    var done = false;
+
+    this.setup = function(){
+        document.getElementsByTagName("body")[0].innerHTML = document.getElementsByTagName("body")[0].innerHTML;
+        if (!done) {
+            done = true;
+            completeResult = document.getElementById("results");
+            document.getElementsByTagName("body")[0].innerHTML = document.getElementsByTagName("body")[0].innerHTML;
+        }
+    };
+
+    // setup();
+
+    this.filterSeatClass = function(){
+        document.getElementById("results").innerHTML = completeResult.innerHTML;
+
+        var seatClassName = document.getElementById("seatClassFilter").value;
+        var airlineName = document.getElementById("airlineFilter").value;
+
+        var seatClassFilter = seatClassName.length == 1;
+        var airlineFilter = airlineName.length != 0;
+
+        if (seatClassFilter || airlineFilter) {
+            var finalResultsHtml = "";
+            var elements = document.getElementsByClassName("results-form");
+
+            for(var i = 0; i < elements.length; i++) {
+                var nodes = elements[i].getElementsByClassName("col-md-6 place-middle font-small lbl-color-light-gray pull-right");
+                var node = nodes[0];
+                var tempSeatClass = node.getElementsByTagName("span")[3].innerHTML.replace(/\s/g,'');
+                nodes = elements[i].getElementsByClassName("col-md-3 place-middle font-large lbl-color-black pull-right");
+                node = nodes[0];
+                var tempAirline = (node.innerHTML.match(/\S+/g) || [])[0];
+
+                if((!seatClassFilter || (seatClassName == tempSeatClass)) && (!airlineFilter || (airlineName == tempAirline)))
+                    finalResultsHtml += elements[i].outerHTML;
+            }
+
+            document.getElementById("results").innerHTML = finalResultsHtml;
+        }
+    };
+
+    this.ascendingCompare = function(a, b){
+        var nodes = a.getElementsByClassName("col-md-12 place-middle");
+        var node = nodes[0];
+        var priceA = node.getElementsByTagName("span")[0].innerHTML;
+
+        nodes = b.getElementsByClassName("col-md-12 place-middle");
+        node = nodes[0];
+        var priceB = node.getElementsByTagName("span")[0].innerHTML;
+
+        return priceA - priceB;
+    };
+
+    this.ascendingSortBasedOnPrice = function(){
+        var elements = document.getElementsByClassName("results-form");
+        elements = Array.prototype.slice.call(elements, 0);
+        elements.sort(ascendingCompare);
+        var finalResultsHtml = "";
+        for(var i = 0; i < elements.length; i++)
+            finalResultsHtml += elements[i].outerHTML;
+        document.getElementById("results").innerHTML = finalResultsHtml;
+    };
+
+    this.descendingCompare = function(a, b){
+        var nodes = a.getElementsByClassName("col-md-12 place-middle");
+        var node = nodes[0];
+        var priceA = node.getElementsByTagName("span")[0].innerHTML;
+
+        nodes = b.getElementsByClassName("col-md-12 place-middle");
+        node = nodes[0];
+        var priceB = node.getElementsByTagName("span")[0].innerHTML;
+
+        return priceB - priceA;
+    };
+
+    this.descendingSortBasedOnPrice = function(){
+        var elements = document.getElementsByClassName("results-form");
+        elements = Array.prototype.slice.call(elements, 0);
+        elements.sort(descendingCompare);
+        var finalResultsHtml = "";
+        for(var i = 0; i < elements.length; i++)
+            finalResultsHtml += elements[i].outerHTML;
+        document.getElementById("results").innerHTML = finalResultsHtml;
+    };
+
+
 });
 
 app.controller('ReserveController', function($scope, $http){
