bdeba76b5762acb8b43da1101de7425f3d98a44e
MelikaAyoughi <melikaayoughi@gmail.com>
2017-05-04 19:56:29 +0430

    flightprovider and seatclassprovider interfaces are complete now

--- src/main/java/domain/FlightRepository.java
+++ src/main/java/domain/FlightRepository.java
@@ -1,7 +1,14 @@
 package domain;
 
+import java.util.ArrayList;
+
 /**
  * Created by Ali_Iman on 5/2/17.
  */
 public interface FlightRepository {
+    void storeFlight(Flight flight);
+    Flight getFlight(String airlineCode, String flightNumber, String date, String srcCode, String destCode);
+    ArrayList<Flight> searchFlights(String date, String srcCode, String destCode);
+    void updateFlight(Flight flight);
+
 }
--- src/main/java/domain/SeatClassRepository.java
+++ src/main/java/domain/SeatClassRepository.java
@@ -4,4 +4,7 @@ package domain;
  * Created by Ali_Iman on 5/2/17.
  */
 public interface SeatClassRepository {
+    void storeSeatClass(SeatClass seatClass);
+    SeatClass getSeatClass(String name, String originCode, String destinationCode, String airlineCode);
+    void updateSeatClass(SeatClass seatClass);
 }
