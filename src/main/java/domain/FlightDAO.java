e3a11dff2d69a468de17f8310c5ab1b258c9da95
MelikaAyoughi <melikaayoughi@gmail.com>
2017-05-05 11:37:43 +0430

    using searchservice to test flightdao. now flight dao has a fake store because no seatclasses are stored and i should write rpepared statement

--- src/main/java/domain/FlightDAO.java
+++ src/main/java/domain/FlightDAO.java
@@ -1,13 +1,40 @@
 package domain;
 
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
  * Created by melikaayoughi on 5/4/17.
  */
 public class FlightDAO implements FlightRepository {
-    public void storeFlight(Flight flight) {
+    private Logger logger = Logger.getLogger(FlightDAO.class);
 
+    //it's better to have store flights so we could use prepared statements
+    public void storeFlight(Flight flight) {
+        DBConnection dbConnection = DBConnection.getDbConnection();
+        Connection connection = dbConnection.getConnection();
+
+        String query = "INSERT INTO \"PUBLIC\".\"FLIGHTS\"\n" +
+                "( \"AIRLINECODE\", \"FLIGHTNUMBER\", \"DATE\", \"SRCCODE\", \"DESTCODE\", \"DEPARTURETIME\", \"ARRIVALTIME\", \"AIRPLANEMODEL\", \"LASTUPDATEDATE\" )\n" +
+                "VALUES ('"+flight.getAirlineCode()+"', '"+flight.getFlightNumber()+"', '"+flight.getDate()+"', '"+flight.getSrcCode()+"', '"+flight.getDestCode()+"', '"+flight.getDepartureTime()+"', '"+flight.getArrivalTime()+"', '"+flight.getAirplaneModel()+"', CURRENT_TIMESTAMP)";
+
+
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+            logger.debug("Flight is inserted into Flights table.logger");
+            System.out.println("Flight is inserted into Flights table.");
+            statement.close();
+            dbConnection.closeConnection(connection);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public Flight getFlight(String airlineCode, String flightNumber, String date, String srcCode, String destCode) {
@@ -15,6 +42,7 @@ public class FlightDAO implements FlightRepository {
     }
 
     public ArrayList<Flight> searchFlights(String date, String srcCode, String destCode) {
+
         return null;
     }
 
--- src/main/java/services/searchService/searchService.java
+++ src/main/java/services/searchService/searchService.java
@@ -1,14 +1,18 @@
 package services.searchService;
 
-import domain.AkbarTicket;
-import domain.Flight;
+import domain.*;
 import services.searchService.searchRequest;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by melikaayoughi on 4/26/17.
@@ -20,8 +24,42 @@ public class searchService {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response getFlights(searchRequest searchReq) throws IOException {
+
         AkbarTicket akbarTicket = AkbarTicket.getAkbarTicket();
-        ArrayList<Flight> flightsList = akbarTicket.search(searchReq.getSrcCode(), searchReq.getDestCode(),searchReq.getDate(),searchReq.getAdultCount(),searchReq.getChildCount(),searchReq.getInfantCount());
-        return Response.status(200).entity(flightsList).build();
+        ArrayList<Flight> flights = akbarTicket.search("THR", "MHD", "05Feb", 0, 1, 0);
+
+        FlightDAO flightDAO = new FlightDAO();
+        for(Flight flight : flights){
+            flightDAO.storeFlight(flight);
+        }
+
+//        DBConnection dbConnection = DBConnection.getDbConnection();
+//        Connection con = dbConnection.getConnection();
+//
+//
+//
+//        try {
+//            Statement statement = con.createStatement();
+//            ResultSet resultSet = null;
+//            resultSet = statement.executeQuery
+//                    ("select count(*) from users where username='melika' and password='ayoughi'");
+//
+//            resultSet.next();
+//            boolean result = resultSet.getInt(1) != 0;
+//
+//            System.out.println("results is: "+result);
+//
+//            resultSet.close();
+//            statement.close();
+//            dbConnection.closeConnection(con);
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        AkbarTicket akbarTicket = AkbarTicket.getAkbarTicket();
+//        ArrayList<Flight> flightsList = akbarTicket.search(searchReq.getSrcCode(), searchReq.getDestCode(),searchReq.getDate(),searchReq.getAdultCount(),searchReq.getChildCount(),searchReq.getInfantCount());
+        return Response.status(200).entity(searchReq).build();
     }
 }
