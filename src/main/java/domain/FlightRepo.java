80d6fa1759044482ddf56b7229784040231052b1
Ali Iman <ali.iman.1995@gmail.com>
2017-05-05 11:28:35 +0430

    implemented FlightRepo (FlightRepo implements FlightRepository and will be used for testing)

--- src/main/java/domain/FlightRepo.java
+++ src/main/java/domain/FlightRepo.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Created by Ali_Iman on 3/3/17.
  */
-public class FlightRepo {
+public class FlightRepo implements FlightRepository{
     private static FlightRepo flightRepo;
     private ArrayList<Flight> flights;
 
@@ -18,42 +18,37 @@ public class FlightRepo {
         return flightRepo;
     }
 
-    public ArrayList<Flight> getFlights() {
+
+    public void storeFlights(ArrayList<Flight> flights) {
+        for(Flight flight : flights) {
+            flight.setFlightId(flights.size());
+            flights.add(flight);
+        }
+        return;
+    }
+
+
+    public ArrayList<Flight> searchFlights(String date, String srcCode, String destCode) {
+        ArrayList<Flight> flights = new ArrayList<Flight>();
+        for(Flight flightEntry : flights)
+            if(flightEntry.getDate().equals(date) &&
+                    flightEntry.getSrcCode().equals(srcCode) && flightEntry.getDestCode().equals(destCode))
+                flights.add(flightEntry);
+        if(flights.size()==0)
+            return null;
         return flights;
     }
 
-    private Flight get(Flight flight) {
+
+    public void updateFlight(Flight flight) {
         for(Flight flightEntry : flights)
-            if(flightEntry.equals(flight))
-                return flightEntry;
-        return null;
-    }
-
-    private boolean alreadyExists(Flight flight) {
-        for(Flight flightEntry : flights)
-            if(flightEntry.equals(flight))
-                return true;
-        return false;
-    }
-
-    private Flight update(Flight flight) {
-        Flight flightEntry = get(flight);
-        flightEntry.setDepartureTime(flight.getDepartureTime());
-        flightEntry.setArrivalTime(flight.getArrivalTime());
-        flightEntry.setAirplaneModel(flight.getAirplaneModel());
-        flightEntry.setMapSeatClassCapacities(flight.getMapSeatClassCapacities());
-        return  flightEntry;
-    }
-
-    private Flight add(Flight flight) {
-        flight.setFlightId(flights.size());
-        flights.add(flight);
-        return flight;
-    }
-
-    public Flight store(Flight flight) {
-        if (alreadyExists(flight))
-            return update(flight);
-        return add(flight);
+            if(flightEntry.equals(flight)) {
+                flightEntry.setArrivalTime(flight.getArrivalTime());
+                flightEntry.setDepartureTime(flight.getDepartureTime());
+                flightEntry.setAirplaneModel(flight.getAirplaneModel());
+                for(Integer i=0; i<flightEntry.getMapSeatClassCapacities().size(); i++)
+                    flightEntry.getMapSeatClassCapacities().get(i).setCapacity(flight.getMapSeatClassCapacities().get(i).getCapacity());
+            }
+        return;
     }
 }
